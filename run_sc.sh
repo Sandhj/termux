7322eda4e3b01d6eebb6db6910529fd5cdd8426b
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-11-18 21:18:15 +0800

    Create run_sc.sh

---
+++ run_sc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Pindah ke direktori
+cd ~/termux/sc
+
+# Cek apakah cf-manager.py sudah running
+if pgrep -f "python sc-installer.py" > /dev/null; then
+    echo "sc-installer.py sudah running"
+    exit 0
+else
+    echo "sc-installer.py belum running, menjalankan..."
+    python sc-installer.py
+fi
