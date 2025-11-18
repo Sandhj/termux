7e8aebafe0f2539624b196d756f8c2b6ac0335ac
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-11-18 21:22:17 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+wget -q https://raw.githubusercontent.com/Sandhj/termux/main/cf/cf-manager.py
+wget -q https://raw.githubusercontent.com/Sandhj/termux/main/sc/sc-installer.py
+wget -q https://raw.githubusercontent.com/Sandhj/termux/main/run_cf.sh
+wget -q https://raw.githubusercontent.com/Sandhj/termux/main/run_sc.sh
