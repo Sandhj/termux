c899e922d4fe5b8ce0d38a3c8087074dcdcd79d7
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-11-18 21:26:03 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
+mkdir -p cf
+mkdir -p sc
+
+cd cf
 wget -q https://raw.githubusercontent.com/Sandhj/termux/main/cf/cf-manager.py
+cd
+cd sc
 wget -q https://raw.githubusercontent.com/Sandhj/termux/main/sc/sc-installer.py
+
+cd
 wget -q https://raw.githubusercontent.com/Sandhj/termux/main/run_cf.sh
 wget -q https://raw.githubusercontent.com/Sandhj/termux/main/run_sc.sh
