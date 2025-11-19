666af540aa4f8c51bfa1f49d87d94a19ea283d6c
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-11-20 07:21:21 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,14 +6,20 @@ wget -q https://raw.githubusercontent.com/Sandhj/termux/main/cf/cf-manager.py
 cd
 cd sc
 wget -q https://raw.githubusercontent.com/Sandhj/termux/main/sc/sc-installer.py
+cd
+cd jam-dg
+wget -q https://raw.githubusercontent.com/Sandhj/termux/main/jam-dg/server.py
+wget -q https://raw.githubusercontent.com/Sandhj/termux/main/jam-dg/index.html
 
 cd
 wget -q https://raw.githubusercontent.com/Sandhj/termux/main/run_cf.sh
 wget -q https://raw.githubusercontent.com/Sandhj/termux/main/run_sc.sh
+wget -q https://raw.githubusercontent.com/Sandhj/termux/main/run_jam-dg.sh
 
 {
   echo "*/1 * * * * bash ~/run_cf.sh"
   echo "*/1 * * * * bash ~/run_sc.sh"
+  echo "*/5 * * * * bash ~/run_jam-dg.sh"
 } | crontab -
 
 crond
